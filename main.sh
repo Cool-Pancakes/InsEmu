b77e20df7e175553332ce599e167f78221a1bf91
Cool-Pancakes <110803123+Cool-Pancakes@users.noreply.github.com>
2023-10-04 16:54:39 +1300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,11 @@
 echo "Welcome to InsEmu"
 echo "What do you want to emulate?"
 echo "type 1 to emulate windows (.exe)"
-read -p " " ans1
+echo -n ": "
+read ans1
+if [[ $ans1 -eq 1 ]]
+then
+  echo "Widows"
+else [[ $ans1 -eq 2 ]]
+  echo "No Widows"
+fi
