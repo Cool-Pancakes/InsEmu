b2f789d50973f4670271264244484d9b5363983f
Cool-Pancakes <110803123+Cool-Pancakes@users.noreply.github.com>
2023-10-04 17:05:00 +1300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,14 @@ echo -n ": "
 read ans1
 if [[ $ans1 -eq 1 ]]
 then
-  echo "Widows"
+  sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+echo "deb https://dl.winehq.org/wine-builds/debian/ bullseye main" | sudo tee /etc/apt/sources.list.d/wine_chrome_os_made_simple.list
+sudo apt update -y
+sudo apt install --install-recommends winehq-stable winbind
+winecfg
+sudo apt install nemo
 else [[ $ans1 -eq 2 ]]
-  echo "No Widows"
+  echo "Please type one of the listed items"]
 fi
