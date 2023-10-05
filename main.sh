9a6dd047a86da1a52f8d7899fbc3b5c826320dd3
Cool-Pancakes <110803123+Cool-Pancakes@users.noreply.github.com>
2023-10-06 10:45:03 +1300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ echo "What do you want to emulate?"
 echo "type 1 to emulate windows (.exe)"
 echo "type 2 to emulate wii/gamecube (.iso and .wad)"
 echo "type 3 to emulate xbox (.iso)
-echo "Add a "J" to the start of the number (J2)"
+echo "Add a J to the start of the number (J2)"
 echo -n ": "
 read ans1
 if [[ $ans1 -eq 1 ]]
