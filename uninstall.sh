fbc919d485ebe28f31df2d78542abd69e5c88313
Cool-Pancakes <110803123+Cool-Pancakes@users.noreply.github.com>
2023-11-03 11:45:44 +1300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 sudo flatpak uninstall flathub app.xemu.xemu
 sudo flatpak uninstall flathub org.DolphinEmu.dolphin-emu
 rm -rf ~/.wine
+echo"Also, to uninstall godot, go into your apps and right click on godot then click uninstall"
 ./InsEmu/main.sh
